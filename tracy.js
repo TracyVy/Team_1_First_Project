ec0a28dee479c8ec93ab2b0419e06589af3552fc
Tracy Vy <tracyvy88@gmail.com>
2020-05-11 19:29:46 -0700

    add prod_id

--- tracy.js
+++ tracy.js
@@ -1,6 +1,15 @@
-// Use of "browser payment APIs"
-var stripe = Stripe("pk_test_P7Plzb4azqJyY0AykXyzua5G00pV8ZeRVp");
-var elements = stripe.elements();
+// Use of "browser payment APIs". Get doc ready for jQuery
+var stripeAPI = Stripe("pk_test_P7Plzb4azqJyY0AykXyzua5G00pV8ZeRVp");
+var yesOnCall = document.getElementById("select_oC");
+var yesBasic = document.getElementById("select_dB");
+var yesExpert = document.getElementById("select_dX");
+var prodOnCall = "prod_HGIp8SvMNtQeca";
+var prodBasic = "prod_HGCJfczaUxxEbY";
+var prodExpert = "prod_HGCNajIFmRf5Mm"
+
+function getProdId() {
+  
+}
 var paymentRequest = stripe.paymentRequest({
   country: "US",
   currency: "usd",
@@ -12,3 +21,21 @@ var paymentRequest = stripe.paymentRequest({
   requestPayerEmail: true,
   // requestPayerPhone: true,
 });
+
+$(document).on("click", "#select_oC", function (){
+  $.ajax({
+    type: "GET",
+    url: "https://api.stripe.com/v1/products/${prodOnCall}",
+    datatype: "JSON",
+  }).then(function (response) {
+    console.log(response);
+})
+$(document).ready(function () {
+ 
+  $.ajax({
+    type: "GET",
+    url: "https://api.stripe.com/v1/products/${prod_id}",
+    datatype: "JSON",
+  }).then(function (response) {
+    console.log(response);
+    
