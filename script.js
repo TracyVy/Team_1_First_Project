58a39afd55517f142b6b55b7a2b2d3fe22bc8ce0
Tracy Vy <tracyvy88@gmail.com>
2020-05-13 15:28:39 -0700

    delete success.html

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 // Use of "browser payment APIs". Get doc ready for jQuery
-/*var stripeAPI = Stripe("pk_test_P7Plzb4azqJyY0AykXyzua5G00pV8ZeRVp");
+var stripe_pk_API = Stripe("pk_test_P7Plzb4azqJyY0AykXyzua5G00pV8ZeRVp");
+// var secretKey = Stripe("sk_test_ouWhRvyXxMFfgerkgNAERERC00SEIgzWxm");
 
 $(document).ready(function () {
-  var id = "";
-})
+  var invoiceID = "";
+
   $.ajax({
     type: "GET",
-    url: "https://api.stripe.com/v1/products/${id}",
+    url: "https://api.stripe.com/v1/",
     datatype: "JSON",
   }).then(function (response) {
     console.log(response);
-    confirmationID = response.id.name;
+    invoiceID = response.invoice.id;
   });
-  $("#receipt_id").append(`<p>Confirmation #: ${id} . Please keep for your records.</p>`);
-  });
+  $("#receipt_id").append(
+    `<p>Confirmation #: ${invoiceID} . Please keep for your records.</p>`
+  );
 });
-*/
